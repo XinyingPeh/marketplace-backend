2c0f4a1223c104bf85daee9f614dbcd04c8529b5
Xinying Peh <jupitoria@gmail.com>
2023-07-05 21:26:28 +0800

    create ItemModel schema and seed file with successful seeding

---
+++ models/ItemModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const ItemSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    description: { type: String },
+    image: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Item = mongoose.model("Item", ItemSchema);
+
+module.exports = Item;
