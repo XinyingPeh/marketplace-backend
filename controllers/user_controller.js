c4dc42513c60b30c0cd2daecb73a1ce82cb32c3a
Xinying Peh <jupitoria@gmail.com>
2023-07-07 14:07:48 +0800

    get login data, do login data validation

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,12 +1,14 @@
 const bcrypt = require("bcrypt");
 const userModel = require("../models/UserModel");
 const userRegistrationValidators = require("./validators/userRegistrationValidator");
+const userLoginValidators = require("./validators/userLoginValidator");
 
 const userControllers = {
   register: async (req, res) => {
     // get the registration data
     const data = req.body;
 
+    // validate the registration data
     const validationResult =
       userRegistrationValidators.registerSchema.validate(data);
     if (validationResult.error) {
@@ -52,5 +54,51 @@ const userControllers = {
 
     // return response
     res.json();
+  },
+
+  login: async (req, res) => {
+    // get the login data
+    const data = req.body;
+
+    // validate the login data
+    const validationResult = userLoginValidators.loginSchema.validate(data);
+
+    if (validationResult.error) {
+      res.statusCode = 400;
+      return res.json({
+        msg: validationResult.error.details[0].message,
+      });
+    }
+
+    // check if user exists by the user (email), return login error (status 400) if do not exist
+
+    let user = null;
+
+    try {
+      user = await userModel.findOne({ email: data.email });
+    } catch (err) {
+      res.statusCode = 500;
+      return res.json({
+        msg: "error occurred when fetching user",
+      });
+    }
+
+    if (!user) {
+      res.statusCode = 401;
+      return res.json({
+        msg: "login failed, please check login details",
+      });
+    }
+
+    // use bcrypt to compare given password against DB record, return status 401 (unauthorized) if failed
+
+    const validLogin = await bcrypt.compare(data.password, user.password);
+
+    if (!validLogin) {
+      res.statusCode = 401;
+      return res.json({
+        msg: "login failed, please check login details",
+      });
+    }
   },
 };
