569c62cabd97be7ab776af9db1b5d1db202c448a
Xinying Peh <jupitoria@gmail.com>
2023-07-07 16:37:42 +0800

    import authmiddleware so that auth user can edit personal info router

--- routers/user_router.js
+++ routers/user_router.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/user_controller')
+const authMiddleware = require('../middlewares/auth_middleware')
+
 
 router.post('/register', userController.register)
 router.post('/login', userController.login)
+router.patch('/edit/personalinfo', authMiddleware, userController.editpersonalinfo)
 
 module.exports = router
