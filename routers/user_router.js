b7e5933bb57325ef638ff374f477c01cf468a55f
joeygohyh <joeyygoh97@gmail.com>
2023-07-09 11:36:46 +0800

    purchase history

--- routers/user_router.js
+++ routers/user_router.js
@@ -1,12 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const userController = require('../controllers/user_controller')
-const authMiddleware = require('../controllers/middlewares/auth_middleware')
+const express = require("express");
+const router = express.Router();
+const userController = require("../controllers/user_controller");
+const authMiddleware = require("../controllers/middlewares/auth_middleware");
 
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.put("/update", authMiddleware, userController.updateUserDetails);
+router.get(
+  "/purchaseHistory",
+  authMiddleware,
+  userController.viewPurchaseHistory
+);
 
-router.post('/register', userController.register)
-router.post('/login', userController.login)
-router.put('/update', authMiddleware, userController.updateUserDetails);
-
-
-module.exports = router
+module.exports = router;
