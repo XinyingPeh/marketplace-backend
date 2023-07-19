039c284f077bedcea7a025907e8be33a2758c19b
Xinying Peh <jupitoria@gmail.com>
2023-07-19 20:57:40 +0800

    add comment

--- routers/user_router.js
+++ routers/user_router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/user_controller");
 const authMiddleware = require("../controllers/middlewares/auth_middleware");
 
+// router endpoints
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.get("/profile", authMiddleware, userController.viewUserDetails);
