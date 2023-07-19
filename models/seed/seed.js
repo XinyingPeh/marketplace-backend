bdcb47aaf4d421222e6e57d3ad1cb4842a2197f7
Xinying Peh <jupitoria@gmail.com>
2023-07-19 20:50:00 +0800

    remove space

--- models/seed/seed.js
+++ models/seed/seed.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const ItemModel = require("../ItemModel");
 
 // import data to be seeded in db
-
 const ItemsRaw = require("../items"); // -> array of product items
 
 // make a connection to db
