f98c37ad21cea2dae0f82f7e262203f6f2c40bc6
Xinying Peh <jupitoria@gmail.com>
2023-07-19 20:51:37 +0800

    add comments

--- routers/payment_router.js
+++ routers/payment_router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authMiddleware = require("../controllers/middlewares/auth_middleware");
 const paymentController = require("../controllers/payment_controller");
 
-// router endpoints
+// router endpoint
 router.post('/checkout', authMiddleware, paymentController.checkout);
 
 module.exports = router;
